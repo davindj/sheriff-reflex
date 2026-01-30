e20f69a22e332c26459bd37468d031ec11afdeea
Davin DJ <davindx88@gmail.com>
2026-01-30 22:13:14 +0700

    Initial Commit

---
+++ ai-portfolio/ai_portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_portfolioApp.swift
+//  ai-portfolio
+//
+//  Created by Davin Djayadi on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ai_portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
